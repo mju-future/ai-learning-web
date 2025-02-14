97baa200ab805539a984cf1d0ed165bcb077d43e
k-kbk <kkbk0077@gmail.com>
2025-02-14 23:14:48 +0900

    feat: textarea 옵션 추가

--- src/app/writing/new/page.tsx
+++ src/app/writing/new/page.tsx
@@ -1,8 +1,15 @@
 'use client';
 
+import { useState } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 
 export default function WritingNew() {
+  const [text, setText] = useState('');
+
+  function handleChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setText(event.target.value);
+  }
+
   return (
     <>
       <h1 className="mt-12 text-3xl font-bold">새 글쓰기</h1>
@@ -14,9 +21,13 @@ export default function WritingNew() {
           autoFocus={true}
           spellCheck={false}
           minRows={12}
+          maxLength={3000}
+          value={text}
+          onChange={handleChange}
         />
+        <div className="text-right">{text.length}/3000자</div>
       </div>
-      <button className="mt-5 bg-violet-100 py-3 font-semibold text-violet-600 transition-colors hover:bg-violet-200">
+      <button className="mt-5 bg-violet-100 py-3 font-semibold text-violet-600 outline-none transition-colors hover:bg-violet-200">
         AI 피드백 받기
       </button>
     </>
