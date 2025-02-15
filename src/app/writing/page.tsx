fea656f44517ff16865ccc1798f8fb187c699bad
k-kbk <kkbk0077@gmail.com>
2025-02-15 13:16:07 +0900

    refactor: 기타 수정

--- src/app/writing/page.tsx
+++ src/app/writing/page.tsx
@@ -1,37 +1,47 @@
 import Link from 'next/link';
 
-const dummyList = [
+const dummy = [
   {
+    id: 1,
+    date: '2025.02.15',
+    title:
+      '스스로 문제를 정의하고 해결하는 소프트웨어 엔지니어입니다. 학교생활 중 직접 경험한 불편함을 어쩌구 저쩌구 해결하기 위해',
+  },
+  {
+    id: 2,
+    date: '2025.02.14',
+    title:
+      '스스로 문제를 정의하고 해결하는 소프트웨어 엔지니어입니다. 학교생활 중 직접 경험한 불편함을 어쩌구 저쩌구 해결하기 위해',
+  },
+  {
+    id: 3,
+    date: '2025.02.14',
+    title:
+      ' 소프트웨어 엔지니어입니다. 학교생활 중 직접 경험한 어쩌구 저쩌구 해결하기 위해 어쩌구 저쩌구',
+  },
+  {
+    id: 4,
+    date: '2025.02.14',
+    title:
+      ' 소프트웨어 엔지니어입니다. 학교생활 중 직접 경험한 불편함을 어쩌구 저쩌구 해결하기 위해',
+  },
+  {
+    id: 5,
+    date: '2025.02.13',
+    title:
+      '스스로 문제를 정의하고 해결하는 소프트웨어 엔지니어입니다. 학교생활 중 직접 경험한 불편함을 개선하기 어쩌구 저쩌구 어어어쩌구 저쩌구',
+  },
+  {
+    id: 6,
+    date: '2025.02.13',
+    title:
+      '스스로 문제를 정의하고 해결하는 소프트웨어 엔지니어입니다. 학교생활 중 직접 어쩌구 저쩌구 어어어쩌구 저쩌구',
+  },
+  {
+    id: 7,
     date: '2025.02.12',
     title:
-      '스스로 문제를 정의하고 해결하는 소프트웨어 엔지니어입니다. 학교생활 중 직접 경험한 불편함을...',
-  },
-  {
-    date: '2025.02.13',
-    title:
-      '스스로 문제를 정의하고 해결하는 소프트웨어 엔지니어입니다. 학교생활 중 직접 경험한 불편함을...',
-  },
-  {
-    date: '2025.02.13',
-    title: '스스로 문제를 정의하고 해결하는 소프트웨어 엔지니어입니다. 학교생활 중 직접 경험한...',
-  },
-  {
-    date: '2025.02.14',
-    title:
-      '스스로 문제를 정의하고 해결하는 소프트웨어 엔지니어입니다. 학교생활 중 직접 경험한 불편함을...',
-  },
-  {
-    date: '2025.02.14',
-    title:
-      '스스로 문제를 정의하고 해결하는 소프트웨어 엔지니어입니다. 학교생활 중 직접 경험한 불편함을 개선하기..',
-  },
-  {
-    date: '2025.02.15',
-    title: '스스로 문제를 정의하고 해결하는 소프트웨어 엔지니어입니다. 학교생활 중 직접..',
-  },
-  {
-    date: '2025.02.15',
-    title: '스스로 문제를 정의하고 해결하는 소프트웨어 엔지니어입니다. 학교생활 중 직접...',
+      '스스로 문제를 정의하고 해결하는 소프트웨어 엔지니어입니다. 학교생활 중 직접 어쩌구 저쩌구 해결하기 위해',
   },
 ];
 
@@ -48,13 +58,15 @@ export default function WritingList() {
         </Link>
       </div>
       <ul className="mt-10 flex flex-col">
-        {dummyList.map((item, index) => (
-          <li
-            key={index}
-            className="flex cursor-pointer justify-between px-4 py-6 transition-colors hover:bg-neutral-50"
-          >
-            <Link href={`/writing/${index}`}>{item.title}</Link>
-            <span className="text-neutral-500">{item.date}</span>
+        {dummy.map(({ id, title, date }) => (
+          <li key={id}>
+            <Link
+              href={`/writing/${id}`}
+              className="flex justify-between px-4 py-6 transition-colors hover:bg-neutral-50"
+            >
+              <span className="w-2/3 truncate">{title}</span>
+              <span className="text-neutral-500">{date}</span>
+            </Link>
           </li>
         ))}
       </ul>
