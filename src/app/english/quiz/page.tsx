053a4a7c923f21615cdc82930b9a8676a3d2b43a
Bokyeom <79684339+k-kbk@users.noreply.github.com>
2025-03-03 15:24:50 +0900

    [fix] api 호출 누락 수정 (#21)

--- src/app/english/quiz/page.tsx
+++ src/app/english/quiz/page.tsx
@@ -1,16 +1,20 @@
 import { cookies } from 'next/headers';
 import { fetchRandomQuizzes } from '@/api';
-import { QuizType } from '@/types';
+import { DetailType, QuizType } from '@/types';
 import QuizContainer from '@/components/quiz/quiz-container';
 
 interface QuizParams {
-  searchParams: Promise<{ type: keyof typeof QuizType; amount: number }>;
+  searchParams: Promise<{
+    type: keyof typeof QuizType;
+    detailType: keyof typeof DetailType;
+    amount: number;
+  }>;
 }
 
 export default async function Quiz({ searchParams }: QuizParams) {
-  const { type, amount } = await searchParams;
+  const { type, detailType, amount } = await searchParams;
   const token = (await cookies()).get('ACCESS_TOKEN')!.value;
-  const quizzes = await fetchRandomQuizzes(type, amount, token);
+  const quizzes = await fetchRandomQuizzes(type, detailType, amount, token);
 
   return <QuizContainer type={type} data={quizzes} />;
 }
